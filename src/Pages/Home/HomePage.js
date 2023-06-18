a2e65dbd1b061db90406d3c39e080c56fe32ec44
MohamedMokhtar6 <mohamed01017715716@gmail.com>
2023-06-18 23:31:43 +0300

    fff

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,30 +1,20 @@
-import React from 'react'
-import Navs from '../../Components/NavBar/Navs'
-import Landing from '../../Components/Home/Landing'
-import { Slide } from 'react-reveal'
-import HomeBooksContainer from '../../Components/Home/HomeBooksContainer'
-import ContactUs from '../../Components/Home/ContactUs'
-import State from '../../Components/Home/State'
-
-
+import React from "react";
+import Navs from "../../Components/NavBar/Navs";
+import Landing from "../../Components/Home/Landing";
+import HomeBooksContainer from "../../Components/Home/HomeBooksContainer";
+import ContactUs from "../../Components/Home/ContactUs";
+import State from "../../Components/Home/State";
 
 function HomePage() {
-    return (
-        <>
-            <Navs />
-            <Landing />
-            <Slide left >
-                <HomeBooksContainer />
-            </Slide>
-            <Slide left >
-                <State />
-            </Slide>
-            <Slide left >
-                <ContactUs />
-            </Slide>
-
-        </>
-    )
+  return (
+    <>
+      <Navs />
+      <Landing />
+      <HomeBooksContainer />
+      <State />
+      <ContactUs />
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
